0c9d589da9a23af2637f29842446c3733d50b410
luisantonc <luisalvancha@gmail.com>
2024-10-23 11:29:03 -0400

    Primer Commit - Contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;             // Definimos la version del compilador
+
+// Inicia la logica de mi contrato
+contract hola_mundo {
+    string public saludo = "Hola desde Educateth";      // Declarando el contenido de mi variable saludo
+
+}
+
